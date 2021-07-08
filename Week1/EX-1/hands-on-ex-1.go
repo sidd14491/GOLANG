b871d6143636d644ccf3dfacffb84177f0b76160
sidd14491 <siddharth14491@gmail.com>
2021-07-09 00:08:43 +0530

    Modify a ex-1

--- Week1/EX-1/hands-on-ex-1.go
+++ Week1/EX-1/hands-on-ex-1.go
@@ -1,3 +1,14 @@
+/*Hands on Exercise-1
+1. Using the short-declaration operator, ASSIGN these VALUES to VARIABLES with
+   the IDENTIFIERS "x", "y" and "z"
+   a. 42
+   b. "James Bond"
+   c. tue
+2. Now print the values stored in those variable using
+	a. a single print statement
+	b. multiple print statement
+*/
+
 package main
 
 import "fmt"
@@ -11,14 +22,3 @@ func main() {
 	fmt.Println(y)
 	fmt.Println(z)
 }
-
-/*Hands on Exercise-1
-1. Using the short-declaration operator, ASSIGN these VALUES to VARIABLES with
-   the IDENTIFIERS "x", "y" and "z"
-   a. 42
-   b. "James Bond"
-   c. tue
-2. Now print the values stored in those variable using
-	a. a single print statement
-	b. multiple print statement
-*/
